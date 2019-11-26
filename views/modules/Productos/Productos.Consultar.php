12559cd4dbd8b8acd38c2845d291050ee6c56be3
codeslator <codeslator@gmail.com>
2019-11-26 19:55:44 -0400

    Datatables

--- views/modules/Productos/Productos.Consultar.php
+++ views/modules/Productos/Productos.Consultar.php
@@ -74,5 +74,40 @@
     <script type="application/javascript" src="<?php echo BASE_URL; ?>assets/js/owner.js"></script>
     <script type="application/javascript" src="<?php echo BASE_URL; ?>assets/js/datatables.js"></script>
     <script type="application/javascript" src="<?php echo BASE_URL; ?>assets/js/data/Producto.js"></script>
+    <script>
+        $('#productos-table').DataTable({
+            responsive: true,
+            "scrollX": true,
+            "pageLength": 10,
+            language: {
+                "sProcessing":     "Procesando...",
+                "sLengthMenu":     "Mostrar _MENU_ registros",
+                "sZeroRecords":    "No se encontraron resultados",
+                "sEmptyTable":     "Ningún dato disponible en esta tabla =(",
+                "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix":    "",
+                "sSearch":         "Buscar:",
+                "sUrl":            "",
+                "sInfoThousands":  ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst":    "Primero",
+                    "sLast":     "Último",
+                    "sNext":     "<i class='icon-navigate_next'></i>",
+                    "sPrevious": "<i class='icon-navigate_before'></i>"
+                },
+                "oAria": {
+                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                },
+                "buttons": {
+                    "copy": "Copiar",
+                    "colvis": "Visibilidad"
+                }
+            }
+        });
+    </script>
 </body>
 </html>
