5abec94dfd5da3cc8e9d803b83b3e2727d5528c5
Jhon Beiker <jhonbeiker.ma26@gmail.com>
2021-02-09 18:08:25 -0400

    mis cambios

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,4 @@
 <?php
-define('BASE_URL','http://localhost/project-IA2/');
+define('BASE_URL','http://localhost/ia2/project-IA2/');
 define('DEFAULT_CONTROLLER','Auth');
 define('DEFAULT_ACTION','index');
